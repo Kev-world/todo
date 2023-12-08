a64e3be950ec77a2d8049c40abd5b9354c823ae9
Kev-world <hengkevin03@gmail.com>
2023-12-08 14:05:34 +0700

    hashing password

--- routers/auth.py
+++ routers/auth.py
@@ -1,7 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
 
-@router.get('/auth/')
-async def get_user():
-    return 'yo'
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+# DTO
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+@router.post('/auth/')
+async def create_user(dto: CreateUserRequest):
+    user_model = Users(
+        email = dto.email,
+        username = dto.username,
+        first_name = dto.first_name,
+        last_name = dto.last_name,
+        hashed_password = bcrypt_context.hash(dto.password),
+        role = dto.role,
+        is_active=True
+    )
+    return user_model
