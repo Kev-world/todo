fd4eceeebb9b2881f44e5aa4d6bac56747ad5672
Kev-world <hengkevin03@gmail.com>
2023-12-08 13:58:26 +0700

    adding user model

--- models.py
+++ models.py
@@ -1,5 +1,16 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String(50), unique=True)
+    username = Column(String(25), unique=True)
+    first_name = Column(String(20))
+    last_name = Column(String(20))
+    hashed_password = Column(String(25))
+    is_active = Column(Boolean, default=True)
+    role = Column(String(20))
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -8,3 +19,4 @@ class Todos(Base):
     description = Column(String(100))
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
