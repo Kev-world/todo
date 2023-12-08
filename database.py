2a3e960144d7d8d9840ce4762a4cbdc95611b3c4
Kev-world <hengkevin03@gmail.com>
2023-12-08 11:37:06 +0700

    open conneciton to mysql and inti data model

--- database.py
+++ database.py
@@ -1,10 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL =  'sqlite:///./todos/db'
+load_dotenv()
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+username = os.getenv('DB_USERNAME')
+password = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+database_name = os.getenv('DB_NAME')
+
+SQLALCHEMY_DATABASE_URL =  f'mysql+mysqlconnector://{username}:{password}@{host}/{database_name}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- models.py
+++ models.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, Integer, String, Boolean
 class Todos(Base):
     __tablename__ = 'todos'
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    description = Column(String)
+    title = Column(String(25))
+    description = Column(String(100))
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
